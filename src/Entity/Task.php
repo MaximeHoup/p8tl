5b338e57fedc8bb875442200742ca4455a69f12a
Maxime Houpiarpanin <m.houpiarpanin@gmail.com>
2023-12-22 16:36:36 +0100

    tasks are now linked to users

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -26,6 +26,9 @@ class Task
     #[ORM\Column]
     private ?bool $isDone = null;
 
+    #[ORM\ManyToOne(inversedBy: 'tasks')]
+    private ?User $user = null;
+
     public function __construct()
     {
         $this->createdAt = new \Datetime();
@@ -88,5 +91,17 @@ class Task
     public function toggle($flag)
     {
         $this->isDone = $flag;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
     }
 }
